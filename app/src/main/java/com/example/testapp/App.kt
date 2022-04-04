e9b14d5fd09b32adec7e9f39935910008d5851a5
Nishkarsh Gupta <42236212+Nishkarshgithub@users.noreply.github.com>
2022-04-04 17:19:55 +0530

    Update App.kt

--- app/src/main/java/com/example/testapp/App.kt
+++ app/src/main/java/com/example/testapp/App.kt
@@ -5,7 +5,7 @@ import com.lokalise.sdk.Lokalise
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        Lokalise.init(appContext = this, sdkToken = "0f93e0a1741eae0db2656509bfac621e6c73ca1e", projectId = "47938759623c21161351f6.23905031")
+        Lokalise.init(appContext = this, sdkToken = "YOUR SDK TOKEN", projectId = "YOUR PROJECT ID")
         // Lokalise.setLocale("lv", "LV")
         // Add this only if you want to use pre-release bundles
         Lokalise.isPreRelease = true // Pre-Release // = false // Production
